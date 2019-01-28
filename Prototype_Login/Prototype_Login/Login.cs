7958152097b3bf94282bf6612df19b341b3726e9
delgado <delgadono@etml.educanet2.ch>
2019-01-28 09:52:33 +0100

    envoie du prototype de log

---
+++ Prototype_Login/Prototype_Login/Login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Prototype_Login
+{
+    public partial class Login : Form
+    {
+        MySqlConnection pictionnary = new MySqlConnection("database=db_pictionnary; server=172.16.30.244; user id=root; pwd=root");
+        MySqlCommand cmd;
+
+        public Login()
+        {
+
+            InitializeComponent();
+            cmd = pictionnary.CreateCommand();
+
+        }
+
+        private void lblUserName_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
