787cc1660dd6c6c821b7b1544f991f62ea8a371f
Guillaume Stäubli <staubligu@etml.educanet2.ch>
2019-02-04 09:25:58 +0100

    Ajout du Client_Room

---
+++ Client_Room/Client/Client/Program.cs
@@ -0,0 +1,63 @@
+﻿/// ETML
+/// Auteur:         Guillaume Stäubli
+/// Date:           28.01.2019
+/// Description:    Implémentation de la classe Program
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            IPAddress ipAdressServer = IPAddress.Parse("172.16.30.46");
+            IPAddress ipAdressLocal = IPAddress.Parse("172.16.30.47");
+
+            string username = "staubligu";
+            
+            int serverPort = 50000;
+            
+            Socket socket = client.ConnectSocket(ipAdressServer, serverPort);
+
+            Console.WriteLine("Connection open with " + ipAdressServer + "\n");
+
+            Console.WriteLine("Press enter to send test datas");
+
+            Console.ReadLine();
+
+            client.Send(username + ";", socket);
+
+            Console.WriteLine("Datas sent\n");
+
+            client.Listen(socket);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Recup the ipAdress of the client
+        /// </summary>
+        /// <returns>client's ip adress</returns>
+        public IPAddress RecupIpAdress()
+        {
+            IPAddress[] localIps = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach(IPAddress ip in localIps)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+
+            return new IPAddress(new long());
+        }
+    }
+
+    
+}
