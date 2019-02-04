967cd8ba879f5eddf041af5256e7a00e3f9e84cf
Félix Rafael <felixra@etmlnet.local>
2019-02-04 09:22:15 +0100

    Added finally (hopefully) correct file for the server

---
+++ Server_Room/Network/Sendable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    /// <summary>
+    /// All the object that will communicate eith each other
+    /// </summary>
+    interface Sendable
+    {
+        /// <summary>
+        /// Gets the data of the object to send
+        /// </summary>
+        /// <returns></returns>
+        byte[] GetData();
+
+        /// <summary>
+        /// Updates the data to send
+        /// </summary>
+        void UpdateData();
+    }
+}
